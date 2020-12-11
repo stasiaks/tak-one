ad84df7030cee4a95be790846eff1582c4bd8f01
Sebastian Stasiak <arionw@live.com>
2020-12-11 01:04:36 +0100

    Tests prepared

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,10 @@
 module Lib
     ( someFunc
+    , divisionDigit
     ) where
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+divisionDigit :: Integer -> Integer -> Integer -> Integer
+divisionDigit _ _ _ = 0 -- PLACEHOLDER
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,13 +1,36 @@
+import Lib (divisionDigit)
 import Test.Tasty
+import Test.Tasty.SmallCheck as SC
 import Test.Tasty.HUnit
 
 main :: IO ()
 main = defaultMain tests
 
 tests :: TestTree
-tests = testGroup "Tests"
-    [ testCase "Basic addition as placeholder" $
-        1 + 3 @?= 4
-    , testCase "Basic failure" $
-        2 - 1 @?= 3
+tests = testGroup "Tests" [scProps, unitTests]
+
+scProps = localOption (SmallCheckDepth 10^3) $ testGroup "Properties"
+    [ SC.testProperty "2 / 3, a > 0" $
+        \a -> a > 0  SC.==> divisionDigit a 2 3 == 6
+    , SC.testProperty "10000 / 8, a > 3" $
+        \a -> a > 3 SC.==> divisionDigit a 2 3 == 0
+    , SC.testProperty "1 / 2000, a > 1" $
+        \a -> a > 3 SC.==> divisionDigit a 2 3 == 0
+    ]
+
+unitTests = testGroup "Unit tests"
+    [ testCase "10000 / 8, 1. cyfra" $
+        divisionDigit 1 10000 8 @?= 1
+    , testCase "10000 / 8, 2. cyfra" $
+        divisionDigit 1 10000 8 @?= 2
+    , testCase "10000 / 8, 3. cyfra" $
+        divisionDigit 1 10000 8 @?= 5
+    , testCase "1 / 2000, 1. cyfra" $
+        divisionDigit 1 10000 8 @?= 5
+    , testCase "71 / 700, 1. cyfra" $
+        divisionDigit 1 10000 8 @?= 1
+    , testCase "71 / 700, 2. cyfra" $
+        divisionDigit 1 10000 8 @?= 0
+    , testCase "71 / 700, 3. cyfra" $
+        divisionDigit 1 10000 8 @?= 1
     ]
