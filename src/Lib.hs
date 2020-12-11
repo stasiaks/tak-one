a7b7dd3090074468899e8e0e7d16fa90bfc6b3be
Sebastian Stasiak <arionw@live.com>
2020-12-11 03:52:28 +0100

    Lib implementation

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,10 +1,29 @@
+{-# LANGUAGE BangPatterns #-}
+
 module Lib
-    ( someFunc
-    , divisionDigit
+    ( divisionDigit
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Data.Char
 
-divisionDigit :: Integer -> Integer -> Integer -> Integer
-divisionDigit _ _ _ = 0 -- PLACEHOLDER
+divisionDigit :: Int -> Int -> Int -> Int
+divisionDigit a b c
+    | intRes == "0" = expansionDigitDropping a b c
+    | a <= length intRes = digitToInt $ intRes !! (a-1)
+    | otherwise = expansionDigit a rem c 0
+    where !(i, rem) = divide b c
+          !intRes = show i
+
+expansionDigit :: Int -> Int -> Int -> Int -> Int
+expansionDigit 0 _ _ acc = acc
+expansionDigit !a !b !c _ = expansionDigit (a-1) rem c acc
+    where !(acc, rem) = divide (b*10) c
+
+expansionDigitDropping :: Int -> Int -> Int -> Int
+expansionDigitDropping !a !b !c
+    | acc == 0 = expansionDigitDropping a rem c
+    | otherwise = expansionDigit (a-1) rem c acc
+    where !(acc, rem) = divide (b*10) c
+
+divide :: Int -> Int -> (Int, Int)
+divide !a !b = (a `quot` b, a `mod` b)
--- test/Spec.hs
+++ test/Spec.hs
@@ -13,24 +13,30 @@ scProps = localOption (SmallCheckDepth 10^3) $ testGroup "Properties"
     [ SC.testProperty "2 / 3, a > 0" $
         \a -> a > 0  SC.==> divisionDigit a 2 3 == 6
     , SC.testProperty "10000 / 8, a > 3" $
-        \a -> a > 3 SC.==> divisionDigit a 2 3 == 0
+        \a -> a > 3 SC.==> divisionDigit a 10000 8 == 0
     , SC.testProperty "1 / 2000, a > 1" $
-        \a -> a > 3 SC.==> divisionDigit a 2 3 == 0
+        \a -> a > 3 SC.==> divisionDigit a 1 2000 == 0
     ]
 
 unitTests = testGroup "Unit tests"
     [ testCase "10000 / 8, 1. cyfra" $
         divisionDigit 1 10000 8 @?= 1
     , testCase "10000 / 8, 2. cyfra" $
-        divisionDigit 1 10000 8 @?= 2
+        divisionDigit 2 10000 8 @?= 2
     , testCase "10000 / 8, 3. cyfra" $
-        divisionDigit 1 10000 8 @?= 5
+        divisionDigit 3 10000 8 @?= 5
     , testCase "1 / 2000, 1. cyfra" $
-        divisionDigit 1 10000 8 @?= 5
+        divisionDigit 1 1 2000 @?= 5
     , testCase "71 / 700, 1. cyfra" $
-        divisionDigit 1 10000 8 @?= 1
+        divisionDigit 1 71 700 @?= 1
     , testCase "71 / 700, 2. cyfra" $
-        divisionDigit 1 10000 8 @?= 0
+        divisionDigit 2 71 700 @?= 0
     , testCase "71 / 700, 3. cyfra" $
-        divisionDigit 1 10000 8 @?= 1
+        divisionDigit 3 71 700 @?= 1
+    , testCase "2 / 3, 10^8. cyfra (performance check)" $
+        divisionDigit (10^8) 2 3 @?= 6
+    , testCase "7 / 22, 10^8. cyfra (performance check)" $
+        divisionDigit (10^8) 7 22 @?= 1
+    , testCase "10^5 / 10^5-1, 10^8. cyfra (performance check)" $
+        divisionDigit (10^8) (10^5) ((10^5)-1) @?= 1
     ]
